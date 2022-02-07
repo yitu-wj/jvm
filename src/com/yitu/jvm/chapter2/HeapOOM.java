bfe7bb642d805eab1409109993557ab3b645100a
Wang Jie <2551434573@qq.com>
2022-02-07 17:02:03 +0800

    Java堆内存溢出异常测试

--- src/com/yitu/jvm/chapter2/HeapOOM.java
+++ src/com/yitu/jvm/chapter2/HeapOOM.java
@@ -1,4 +1,20 @@
 package com.yitu.jvm.chapter2;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM Args:-Xms2m -Xmx2m -XX:+HeapDumpOnOutOfMemoryError
+ */
 public class HeapOOM {
+    static class OOMObject{
+
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list=new ArrayList<>();
+        while (true){
+            list.add(new OOMObject());
+        }
+    }
 }
