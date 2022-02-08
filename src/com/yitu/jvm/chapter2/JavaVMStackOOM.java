7d66d220346af6a1aebd9d9dfcfb478a5d59f6fb
Wang Jie <2551434573@qq.com>
2022-02-08 15:37:29 +0800

    创建线程导致内存溢出异常

---
+++ src/com/yitu/jvm/chapter2/JavaVMStackOOM.java
@@ -0,0 +1,28 @@
+package com.yitu.jvm.chapter2;
+
+/**
+ * VM Args: -Xss2M
+ */
+public class JavaVMStackOOM {
+    private void dontStop(){
+        while (true){
+
+        }
+    }
+    public void stackLeakByThread(){
+        while (true){
+            Thread thread=new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    dontStop();
+                }
+            });
+            thread.start();
+        }
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackOOM oom = new JavaVMStackOOM();
+        oom.stackLeakByThread();
+    }
+}
