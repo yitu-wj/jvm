d26eaaf4b232198663b624634005321ac5a5b45b
Wang Jie <2551434573@qq.com>
2022-03-01 17:14:04 +0800

    常量在编译阶段会存入调用类的常量池中，本质上没有直接引用到定义常量的类，因此不会触发定义常量的类的初始化

---
+++ src/com/yitu/jvm/chapter7/ConstClass.java
@@ -0,0 +1,12 @@
+package com.yitu.jvm.chapter7;
+
+/**
+ * 常量在编译阶段会存入调用类的常量池中，本质上没有直接引用到定义常量的类，因此不会触发定义常量的类的初始化
+ */
+public class ConstClass {
+    static {
+        System.out.println("ConstClass init!");
+    }
+
+    public static final String HELLOWORLD = "hello world";
+}
---
+++ src/com/yitu/jvm/chapter7/NotInitialization_3.java
@@ -0,0 +1,7 @@
+package com.yitu.jvm.chapter7;
+
+public class NotInitialization_3 {
+    public static void main(String[] args) {
+        System.out.println(ConstClass.HELLOWORLD);
+    }
+}
